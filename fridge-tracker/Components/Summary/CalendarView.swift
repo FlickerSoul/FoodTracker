e21f047158c992b15d603f0cb770886a5c0ba7f2
Larry Zeng <hi@universe.observer>
2023-06-10 19:44:25 +0100

    feat: add calendar view

--- fridge-tracker/Components/Summary/CalendarView.swift
+++ fridge-tracker/Components/Summary/CalendarView.swift
@@ -5,14 +5,94 @@
 //  Created by Heyuan Zeng on 6/9/23.
 //
 
+import Charts
 import SwiftUI
 
+#if DEBUG
+    import SwiftData
+#endif
+
 struct CalendarView: View {
+    #if DEBUG
+        @Query var items: [FridgeItem]
+    #else
+        var items: [FridgeItem]
+    #endif
+
+    private typealias ItemInfoType = [Date: [FridgeItem]]
+
+    private var itemsByDates: ItemInfoType {
+        return Dictionary(grouping: items) { item in
+            roundDownToDate(date: item.expiryDate)
+        }
+    }
+
+    private typealias ItemCountTupleType = (date: Date, count: Int)
+
+    private var itemCountByDates: [ItemCountTupleType] {
+        return itemsByDates.map { date, items in
+            (date: date, count: items.count)
+        }
+    }
+
+    private var maxCount: Int {
+        return itemCountByDates.reduce(Int.min) { res, countInfo in
+            max(res, countInfo.count)
+        }
+    }
+
+    @State var scrollPosition: Int = 0
+
     var body: some View {
-        Text("calendar")
+        GeometryReader { geometry in
+            VStack {
+                Chart {
+                    ForEach(itemCountByDates, id: \.date) { date, count in
+                        BarMark(
+                            x: .value("Date", date, unit: .day),
+                            y: .value("Item Count", count)
+                        )
+                        .foregroundStyle(date > Date.now ? .mint : .orange)
+                    }
+
+                    RuleMark(x: .value("Today", Date.now, unit: .day))
+                        .foregroundStyle(.yellow)
+                        .offset(yStart: -10)
+                        .annotation(
+                            position: .top,
+                            spacing: 0,
+                            overflowResolution: .init(
+                                x: .fit(to: .chart),
+                                y: .disabled
+                            )
+                        ) {
+                            ZStack {
+                                Rectangle()
+                                    .frame(width: 30, height: 20)
+                                Text("Now")
+                            }
+                        }
+                }
+                .chartScrollableAxes(.horizontal)
+                .chartXAxis {
+                    AxisMarks(values: .stride(by: .day))
+                }
+                .chartXVisibleDomain(length: 7 * SECONDS_IN_A_DAY)
+                .chartYScale(domain: 0 ... (maxCount + 1))
+                .frame(maxHeight: geometry.size.width)
+                .chartScrollPosition(initialX: Date.now - SECONDS_IN_A_DAY)
+                .chartScrollPosition(x: $scrollPosition)
+                .chartYAxisLabel("Food Count")
+                .chartXAxisLabel("Expiry Date")
+                .padding()
+
+                Text("\(scrollPosition)")
+            }
+        }
     }
 }
 
 #Preview {
     CalendarView()
+        .modelContainer(previewContainer)
 }
--- fridge-tracker/Models/SampleFridgeItems.swift
+++ fridge-tracker/Models/SampleFridgeItems.swift
@@ -12,26 +12,34 @@ struct SampleFridgeItems {
             name: "cake",
             note: "brought today",
             addedDate: Date.now,
-            expiryDate: Date.now + 2 * 24 * 3600
+            expiryDate: Date.now + 2 * SECONDS_IN_A_DAY
         ),
         FridgeItem(
             name: "Banana",
             note: "from a big shopping",
-            addedDate: Date.now - 2 * 24 * 3600,
-            expiryDate: Date.now + 5 * 24 * 3600
+            addedDate: Date.now - 2 * SECONDS_IN_A_DAY,
+            expiryDate: Date.now + 5 * SECONDS_IN_A_DAY
         ),
         FridgeItem(
             name: "yogert",
             note: "also from the big shopping",
-            addedDate: Date.now - 2 * 24 * 3600,
-            expiryDate: Date.now + 1 * 24 * 3600,
+            addedDate: Date.now - 2 * SECONDS_IN_A_DAY,
+            expiryDate: Date.now + 1 * SECONDS_IN_A_DAY,
             notificationOn: false
         ),
         FridgeItem(
             name: "expired",
             note: "also from the big shopping",
-            addedDate: Date.now - 2 * 24 * 3600,
-            expiryDate: Date.now - 1 * 24 * 3600,
+            addedDate: Date.now - 2 * SECONDS_IN_A_DAY,
+            expiryDate: Date.now - 1 * SECONDS_IN_A_DAY,
+            archived: true
+        ),
+
+        FridgeItem(
+            name: "so expired",
+            note: "also from the big shopping",
+            addedDate: Date.now - 10 * SECONDS_IN_A_DAY,
+            expiryDate: Date.now - 5 * SECONDS_IN_A_DAY,
             archived: true
         ),
     ]
