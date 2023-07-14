8a9f15c113cc404cb223f5798a17a67536fdd115
Larry Zeng <hi@universe.observer>
2023-07-14 19:04:40 +0100

    feat: add versioned schema

--- food-tracker/Models/FoodItem.swift
+++ food-tracker/Models/FoodItem.swift
@@ -9,127 +9,138 @@ import Foundation
 import SwiftData
 import UserNotifications
 
-@Model
-final class FoodItem {
-    // swiftformat:disable:next all
+enum FoodTrackerSchemeV1: VersionedSchema {
+    static var versionIdentifier: String? = "0.1.0"
+    
+    static var models: [any PersistentModel.Type] {
+        [Self.FoodItem.self]
+    }
+    
+    @Model
+    final class FoodItem {
+        // swiftformat:disable:next all
     var id: UUID = UUID()
     
-    var name: String = ""
-    var note: String = ""
-    // swiftformat:disable:next all
+        var name: String = ""
+        var note: String = ""
+        // swiftformat:disable:next all
     var addedDate: Date = Date.now
-    // swiftformat:disable:next all
+        // swiftformat:disable:next all
     var expiryDate: Date = Date.now
     
-    var barcode: String = ""
+        var barcode: String = ""
     
-    var notificationOn: Bool = true {
-        didSet {
-            toggleNotification()
+        var notificationOn: Bool = true {
+            didSet {
+                toggleNotification()
+            }
         }
-    }
     
-    var archived: Bool = false {
-        didSet {
-            toggleNotification()
+        var archived: Bool = false {
+            didSet {
+                toggleNotification()
+            }
         }
-    }
 
-    @Attribute private var notificationIdentifiersEncoded: String = "" // TODO: ehhhh how to do transformable?
+        @Attribute private var notificationIdentifiersEncoded: String = "" // TODO: ehhhh how to do transformable?
     
-    @Attribute(.transformable) var notificationIdentifiers: [String] {
-        get {
-            _$observationRegistrar.access(self, keyPath: \.notificationIdentifiersEncoded)
-            return getValue(for: \.notificationIdentifiersEncoded).components(separatedBy: ",")
+        @Attribute(.transformable) var notificationIdentifiers: [String] {
+            get {
+                _$observationRegistrar.access(self, keyPath: \.notificationIdentifiersEncoded)
+                return getValue(for: \.notificationIdentifiersEncoded).components(separatedBy: ",")
+            }
+            set {
+                _$observationRegistrar.withMutation(of: self, keyPath: \.notificationIdentifiersEncoded) {
+                    self.setValue(for: \.notificationIdentifiersEncoded, to: newValue.joined(separator: ","))
+                }
+            }
         }
-        set {
-            _$observationRegistrar.withMutation(of: self, keyPath: \.notificationIdentifiersEncoded) {
-                self.setValue(for: \.notificationIdentifiersEncoded, to: newValue.joined(separator: ","))
+    
+        // swiftformat:disable:next all
+    var category: FoodItemCategory = FoodItemCategory.None  // SwiftData bug, enum changes through Picker will not be persistent 
+    
+        var isTemplate: Bool = false
+    
+        var quantity: Int = 1
+    
+        private var notificationScheduled: Bool {
+            return !notificationIdentifiers.isEmpty
+        }
+    
+        func toggleNotification() {
+            if notificationOn, !archived {
+                NotificationHandler.current.requestNotification()
+                NotificationHandler.current.scheduleNotification(item: self)
+            } else {
+                NotificationHandler.current.cancelNotification(item: self)
+            }
+        }
+    
+        init(name: String, barcode: String = "", note: String = "", addedDate: Date = Date.now, expiryDate: Date = Date.now, notificationOn: Bool = true, archived: Bool = false, notificationIdentifiers: [String] = [], category: FoodItemCategory = .None, isTemplate: Bool = false, quantity: Int = 1) {
+            self.id = UUID()
+            self.name = name
+            self.barcode = barcode
+            self.note = note
+            self.addedDate = addedDate
+            self.expiryDate = expiryDate
+            self.notificationIdentifiers = notificationIdentifiers
+            self.notificationOn = notificationOn
+            self.archived = archived
+            self.category = category
+            self.isTemplate = isTemplate
+            self.quantity = quantity
+        }
+    
+        static func makeDefaultFoodItem(name: String = "") -> Self {
+            return Self(name: name)
+        }
+    
+        static func isExpiring(item: FoodItem) -> Bool {
+            return item.expiryDate < Date.now
+        }
+    
+        func copy() -> Self {
+            return .init(
+                name: name,
+                barcode: barcode,
+                note: note,
+                addedDate: addedDate,
+                expiryDate: expiryDate,
+                notificationOn: notificationOn,
+                archived: archived,
+                notificationIdentifiers: notificationIdentifiers,
+                category: category,
+                isTemplate: isTemplate,
+                quantity: quantity
+            )
+        }
+    
+        func copy(by style: TemplateCreationStyle) -> Self {
+            switch style {
+            case .sameExpiryDate:
+                return .init(
+                    name: name,
+                    barcode: barcode,
+                    note: note,
+                    expiryDate: expiryDate,
+                    category: category,
+                    quantity: quantity
+                )
+            case .samePeriodOfTime:
+                let now = Date.now
+                return .init(
+                    name: name,
+                    barcode: barcode,
+                    note: note,
+                    addedDate: now,
+                    expiryDate: now + expiryDate.timeIntervalSince(addedDate),
+                    category: category,
+                    quantity: quantity
+                )
             }
         }
     }
-    
-    // swiftformat:disable:next all
-    var category: FoodItemCategory = FoodItemCategory.None  // SwiftData bug, enum changes through Picker will not be persistent 
-    
-    var isTemplate: Bool = false
-    
-    var quantity: Int = 1
-    
-    private var notificationScheduled: Bool {
-        return !notificationIdentifiers.isEmpty
-    }
-    
-    func toggleNotification() {
-        if notificationOn, !archived {
-            NotificationHandler.current.requestNotification()
-            NotificationHandler.current.scheduleNotification(item: self)
-        } else {
-            NotificationHandler.current.cancelNotification(item: self)
-        }
-    }
-    
-    init(name: String, barcode: String = "", note: String = "", addedDate: Date = Date.now, expiryDate: Date = Date.now, notificationOn: Bool = true, archived: Bool = false, notificationIdentifiers: [String] = [], category: FoodItemCategory = .None, isTemplate: Bool = false, quantity: Int = 1) {
-        self.id = UUID()
-        self.name = name
-        self.barcode = barcode
-        self.note = note
-        self.addedDate = addedDate
-        self.expiryDate = expiryDate
-        self.notificationIdentifiers = notificationIdentifiers
-        self.notificationOn = notificationOn
-        self.archived = archived
-        self.category = category
-        self.isTemplate = isTemplate
-        self.quantity = quantity
-    }
-    
-    static func makeDefaultFoodItem(name: String = "") -> Self {
-        return Self(name: name)
-    }
-    
-    static func isExpiring(item: FoodItem) -> Bool {
-        return item.expiryDate < Date.now
-    }
-    
-    func copy() -> Self {
-        return .init(
-            name: name,
-            barcode: barcode,
-            note: note,
-            addedDate: addedDate,
-            expiryDate: expiryDate,
-            notificationOn: notificationOn,
-            archived: archived,
-            notificationIdentifiers: notificationIdentifiers,
-            category: category,
-            isTemplate: isTemplate,
-            quantity: quantity
-        )
-    }
-    
-    func copy(by style: TemplateCreationStyle) -> Self {
-        switch style {
-        case .sameExpiryDate:
-            return .init(
-                name: name,
-                barcode: barcode,
-                note: note,
-                expiryDate: expiryDate,
-                category: category,
-                quantity: quantity
-            )
-        case .samePeriodOfTime:
-            let now = Date.now
-            return .init(
-                name: name,
-                barcode: barcode,
-                note: note,
-                addedDate: now,
-                expiryDate: now + expiryDate.timeIntervalSince(addedDate),
-                category: category,
-                quantity: quantity
-            )
-        }
-    }
 }
+
+typealias LatestSchema = FoodTrackerSchemeV1
+typealias FoodItem = LatestSchema.FoodItem
